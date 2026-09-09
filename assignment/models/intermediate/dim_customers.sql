36cab9ab8cabf7d03eeffee82a158bc0804e3a3c
ivansurachman <ivansurachman.work@gmail.com>
2026-09-09 17:06:48 +0700

    Refactor customer dimension SQL to improve readability and maintainability

--- assignment/models/intermediate/dim_customers.sql
+++ assignment/models/intermediate/dim_customers.sql
@@ -9,19 +9,19 @@ stg_payments as (
     select * from {{ ref('stg_payments') }}
 )
 select 
-    customer_id,
-    concat(first_name, ' ', last_name) as customer_name,
+    c.customer_id,
+    concat(c.first_name, ' ', c.last_name) as customer_name,
     case 
-        when count(rental_id) is null then 0
-        else count(rental_id)
+        when count(r.rental_id) is null then 0
+        else count(r.rental_id)
     end as total_rentals,
-    min(rental_at) as first_rented_at,
-    max(rental_at) as last_rented_at,
+    min(r.rental_at) as first_rented_at,
+    max(r.rental_at) as last_rented_at,
     case 
-        when sum(amount) is null then 0
-        else sum(amount)
+        when sum(p.amount) is null then 0
+        else sum(p.amount)
     end as lifetime_payment_total
-from stg_customers
-left join stg_rentals on stg_customers.customer_id = stg_rentals.customer_id
-left join stg_payments on stg_customers.customer_id = stg_payments.customer_id
-group by customer_id, customer_name
+from stg_customers c
+left join stg_rentals r on c.customer_id = r.customer_id
+left join stg_payments p on c.customer_id = p.customer_id
+group by c.customer_id, customer_name
