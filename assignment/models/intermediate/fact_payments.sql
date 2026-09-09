ffc130633bb2d794452becfcc2af4320adcd36d9
ivansurachman <ivansurachman.work@gmail.com>
2026-09-09 17:25:26 +0700

    Add fact table for payments with necessary joins and dimensions

---
+++ assignment/models/intermediate/fact_payments.sql
@@ -0,0 +1,33 @@
+-- Fact table for payments
+with stg_payments as (
+    select * from {{ ref('stg_payments') }}
+),
+dim_customers as (
+    select * from {{ ref('dim_customers') }}
+),
+stg_rentals as (
+    select * from {{ ref('stg_rentals') }}
+),
+stg_inventory as (
+    select * from {{ ref('stg_inventory') }}
+),
+dim_film as (
+    select * from {{ ref('dim_films') }}
+)
+select
+    p.payment_id,
+    p.paid_at,
+    p.paid_at::date as paid_date,
+    p.customer_id,
+    c.customer_name,
+    p.staff_id,
+    i.store_id,
+    p.rental_id,
+    r.film_id,
+    f.title as film_title,
+    p.amount
+from stg_payments p
+left join dim_customers c on p.customer_id = c.customer_id
+left join stg_rentals r on p.rental_id = r.rental_id
+left join stg_inventory i on r.inventory_id = i.inventory_id
+left join dim_film f on i.film_id = f.film_id
