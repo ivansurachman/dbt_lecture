e3dccedc16e95dd28abf2e4588bf3e6547cae278
ivansurachman <ivansurachman.work@gmail.com>
2026-09-10 09:26:36 +0700

    Remove unused fact_payments reference from customer performance mart model

--- assignment/models/marts/mart_customer_performance.sql
+++ assignment/models/marts/mart_customer_performance.sql
@@ -1,7 +1,4 @@
 -- Mart model for customer performance
-with fact_payments as (
-    select * from {{ ref('fact_payments') }}
-),
 with dim_customers as (
     select * from {{ ref('dim_customers') }}
 )
@@ -14,4 +11,3 @@ select
     c.lifetime_payment_total,
     c.lifetime_payment_total / nullif(c.total_rentals, 0) as average_payment_value
 from dim_customers c
-left join fact_payments p on c.customer_id = p.customer_id
