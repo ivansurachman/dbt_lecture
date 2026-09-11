1a38b2d2446b57d15622cd6bed1253ec244b7976
ivansurachman <ivansurachman.work@gmail.com>
2026-09-11 11:42:30 +0700

    Enhance daily revenue assertion by including store_id in the join and refining payment aggregation

--- assignment/tests/assert_daily_revenue_matches_payments.sql
+++ assignment/tests/assert_daily_revenue_matches_payments.sql
@@ -1,19 +1,21 @@
 with daily_payments as (
     select
-        paid_at::date as paid_date,
-        sum(amount) as total_revenue,
-        count(distinct payment_id) as total_payments
-    from {{ ref('stg_payments') }}
-    group by 1
+        p.paid_at::date as paid_date,
+        i.store_id,
+        sum(p.amount) as total_revenue,
+        count(p.payment_id) as total_payments
+    from {{ ref('stg_payments') }} p
+    join {{ ref('stg_rentals') }} r on r.rental_id = p.rental_id
+    join {{ ref('stg_inventory') }} i on i.inventory_id = r.inventory_id
+    group by 1,2
 )
-
 select 
     dr.total_revenue,
     dr.total_payments,
     dp.total_revenue as expected_total_revenue,
     dp.total_payments as expected_total_payments
 from {{ ref('mart_daily_revenue') }} dr
-join daily_payments dp on dr.paid_date = dp.paid_date
+join daily_payments dp on dr.paid_date = dp.paid_date and dr.store_id = dp.store_id
 where dr.total_revenue != dp.total_revenue
 or dr.total_payments != dp.total_payments
 or dr.total_revenue is null
